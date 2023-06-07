ada14e3c58c5681230558357ed5fd8ca6899de64
austin <124389297+austin-scavone@users.noreply.github.com>
2023-06-07 19:11:15 +0000

    fixes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a pyramid
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const aBase = params.get('a')
+  const bBase = params.get('b')
+  const height = params.get('h')
+  console.log(aBase)
+
+  // process
+  const area = ((aBase + bBase) / 2) * height
+  const dimensions = "<ul>\n<li>a base = " + aBase + "</li>\n<li>b base = " + bBase + "</li>\n<li>height = " + height + "</li>\n</ul>"
+
+  // output
+  document.getElementById('dimensions').innerHTML = dimensions
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' mm²'
+}
